6b3759b5f3c0c2c20fd19a26b2975ae4b8ebf0ad
PaoloGinefra <ginefra.paolo@gmail.com>
2022-11-18 22:35:34 +0100

    chore: started documenting code

--- main/input.h
+++ main/input.h
@@ -1,23 +1,48 @@
+/*
+========================================================================
+This files handles the buttons.
+
+Every button has its own struct that records its state and a bunch
+of other useful information.
+
+There are three functions. An input Setup and Loop that need to be called
+ in the Setup and Loop of the main file repectively
+========================================================================
+*/
+
 #define B1_PIN 3
 #define B2_PIN 4
 
 typedef struct
 {
     int Pin;
+
+    // True when the button is held down
     bool isDown;
+
+    // True only when the button is pressed
     bool isPressed;
+
+    // True only when the button is released
     bool isReleased;
+
+    // The value of the button on the previous iteration
     bool oldVal;
+
+    // Records if the button has been pressed
     bool toggle;
 } Button_;
 
+// initialize buttons
 Button_ b1 = {B1_PIN};
 Button_ b2 = {B2_PIN};
 
+// Funtions prototype
 void inputSetup();
 void inputLoop();
 void updateButton(Button_ *);
 
+// Actual Functions
 void inputSetup()
 {
     pinMode(b1.Pin, INPUT);
@@ -43,6 +68,7 @@ void updateButton(Button_ *b)
     b->toggle |= b->isPressed;
 }
 
+// This resets the toggle property
 void unToggleButton(Button_ *b)
 {
     b->toggle = false;
